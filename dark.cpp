8320a3a9b9d36f44fe8b5a73fa077a2f7eaf020a
93r <ma_ko@riseup.net>
2017-07-27 21:33:03 +0200

    Add atan2 function

--- dark.cpp
+++ dark.cpp
@@ -2,7 +2,6 @@
 
 namespace dark {
 	void exit(integer i) { std::exit(i); }
-
 }
 
 int main() {
@@ -11,9 +10,10 @@ int main() {
 	dark::integer z = -888;
 	dark::string s = "dark";
 
-	dark::print(x, s, "mako");
-	dark::print(y);
-	dark::print(dark::abs(z));
+	dark::print(x, s, "mako\n");
+	dark::print(y, "\n");
+	dark::print(dark::abs(z), "\n");
+	dark::print(dark::atan2(1.0, 1.0), "\n");
 
 	dark::exit(0);
 	return 0;
